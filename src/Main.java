a78933046e8ca21fd82f9c14f034bedf0c7d63bb
Harald Görtz <harald.gortz@gmail.com>
2013-08-19 17:30:44 +0200

    Added --version option

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,33 @@
 
-import parser.*;
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Properties;
 
-import beaver.Parser.Exception;
-import AST.*;
+import parser.GrammarParser;
+import parser.GrammarScanner;
+import AST.ASTNode;
+import AST.Grammar;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			if (args.length < 2) {
-				System.err.println("Arguments: [--beaver] SourceFileName DestFileName");
-				System.exit(1);
+			
+			if (args[0].equals("--version")) {
+				System.out.println("JastAddParser version " + versionString());
+				System.exit(0);
 			}
 			boolean beaver = false;
-			if (args.length == 3 && args[0].equals("--beaver"))
-				beaver = true;
+			if (args[0].equals("--beaver")) {
+				beaver = true;	
+			}
 			int sourceIndex = args.length == 3 ? 1 : 0;
 			int destIndex = args.length == 3 ? 2 : 1;
 			String source = args[sourceIndex];
@@ -55,9 +66,27 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
-		} catch (Exception e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Usage: java -jar JastAddParser.jar <options> " + 
+					"<source file name> <destination file name>");
+			System.exit(1);
+		} catch (beaver.Parser.Exception e) {
 			System.err.println(e.getMessage());
 			System.exit(1);
 		}
+	}
+
+	private static String versionString() {
+		Properties props = new Properties();
+		String version = null;
+		try {
+			FileInputStream in = new FileInputStream(new File("properties/version.properties"));
+			props.load(in);
+			version = props.getProperty("version");
+		} catch (Exception e) {
+			System.err.println("Could not retrieve version information");
+			System.exit(1);
+		}
+		return version;
 	}
 }
